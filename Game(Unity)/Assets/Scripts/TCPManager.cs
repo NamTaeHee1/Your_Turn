9676c47411b4369f68f94f0d9ca81a7c322b56a4
NamTaeHee1 <skaxogml777@naver.com>
2021-04-06 16:31:33 +0900

    [UPDATE] TCPManager
    [UPDATE] TCPManager

--- Game(Unity)/Assets/Scripts/TCPManager.cs
+++ Game(Unity)/Assets/Scripts/TCPManager.cs
@@ -8,24 +8,21 @@ using UnityEngine;
 public class TCPManager : MonoBehaviour
 {
     TcpClient Client;
-    string serverIP = "127.0.0.1";
+    string ServerIP = "127.0.0.1";
     const int PORT = 8000;
     byte[] ReceveBuffer;
 
-    void Start()
-    {
-        CheckWpfReceive();
-    }
+    void Start() => CheckWpfReceive();
 
     void CheckWpfReceive()
     {
-        Client = new TcpClient(serverIP, PORT);
+        Client = new TcpClient(ServerIP, PORT);
         NetworkStream Stream;
         Stream = Client.GetStream();
 
         ReceveBuffer = new byte[14];
         Stream.Read(ReceveBuffer, 0, ReceveBuffer.Length);
-        string Message = Encoding.UTF8.GetString(ReceveBuffer, 0, ReceveBuffer.Length);
-        Debug.Log(Message);
+        string Message = Encoding.Default.GetString(ReceveBuffer, 0, ReceveBuffer.Length);
+        Debug.Log("Length : " + ReceveBuffer.Length + " Message : " + Message);
     }
 }
