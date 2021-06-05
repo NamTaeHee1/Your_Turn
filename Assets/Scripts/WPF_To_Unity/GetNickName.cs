44e0faacaa5e0cec30a359cec3772e61ba3290e9
NamTaeHee1 <skaxogml777@naver.com>
2021-06-06 00:49:54 +0900

    [ADD] NickNameControl + [DELETE] TCPManager
    [ADD] NickNameControl + [DELETE] TCPManager

---
+++ Assets/Scripts/WPF_To_Unity/GetNickName.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetNickName : MonoBehaviour
+{
+    string NickNameFilePath = @"D:\github\Your_Turn_Client\FileStream\NickName.txt";
+
+    void Start()
+    {
+        
+    }
+
+}
