aed6343c0ef145ab8ae7c963432f6cdd523b09a8
NamTaeHee1 <skaxogml777@naver.com>
2021-06-08 15:20:49 +0900

    [UPDATE] RoomListManager
    [UPDATE] RoomListManager

--- Assets/Scripts/Main/RoomListManager.cs
+++ Assets/Scripts/Main/RoomListManager.cs
@@ -4,6 +4,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class RoomListManager : MonoBehaviourPunCallbacks
 {
@@ -56,8 +57,8 @@ public class RoomListManager : MonoBehaviourPunCallbacks
         for(int i = 0; i < CellButton.Length; i++)
         {
             CellButton[i].transform.GetChild(0).gameObject.SetActive((Multiple + i < MyList.Count) ? true : false);
-            CellButton[i].transform.GetChild(1).GetComponent<Text>().text = (Multiple + i < MyList.Count) ? MyList[Multiple + i].Name : "";
-            CellButton[i].transform.GetChild(2).GetComponent<Text>().text = (Multiple + i < MyList.Count) ? string.Format("ÆÀ´ç {0}¸í", (MyList[Multiple + i].MaxPlayers / 2).ToString()) : "";
+            CellButton[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = (Multiple + i < MyList.Count) ? MyList[Multiple + i].Name : "";
+            CellButton[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = (Multiple + i < MyList.Count) ? string.Format("ÆÀ´ç {0}¸í", (MyList[Multiple + i].MaxPlayers / 2).ToString()) : "";
         }
     }
 
