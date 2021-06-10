1ade835542d1966d9aa72db5d62c6e275fdba611
NamTaeHee1 <skaxogml777@naver.com>
2021-06-10 22:03:26 +0900

    [UPDATE] MainScene + TeamManager
    [UPDATE] MainScene + TeamManager

--- Assets/Scripts/Main/TeamManager.cs
+++ Assets/Scripts/Main/TeamManager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Photon.Pun;
 
-public class TeamManager : MonoBehaviour
+public class TeamManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject[] RedTeamList;
     [SerializeField] GameObject[] BlueTeamList;
