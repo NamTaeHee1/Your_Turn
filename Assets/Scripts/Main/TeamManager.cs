cf43f33480e8f3fbd37189792feb271c4c5d1342
NamTaeHee1 <skaxogml777@naver.com>
2021-06-10 22:15:48 +0900

    [UPDATE] TeamManager
    [UPDATE] TeamManager

--- Assets/Scripts/Main/TeamManager.cs
+++ Assets/Scripts/Main/TeamManager.cs
@@ -7,5 +7,35 @@ using Photon.Pun;
 public class TeamManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject[] RedTeamList;
+    int RedTeamPlayerCount = 0;
     [SerializeField] GameObject[] BlueTeamList;
+    int BlueTeamPlayerCount = 0;
+
+    public override void OnJoinedRoom()
+    {
+        ActivationPanel(RedTeamList[RedTeamPlayerCount++]);
+    }
+
+    public override void OnLeftRoom()
+    {
+
+    }
+
+    void ActivationPanel(GameObject Panel)
+    {
+        Panel.transform.GetChild(0).gameObject.SetActive(true);
+        Panel.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = PhotonNetwork.NickName;
+        Panel.transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    void DisabledPanel(GameObject Panel)
+    {
+
+    }
+
+    void TeamListRenewal()
+    {
+
+    }
+
 }
