8c19071026794c583b8f8d052a660dc38fbad12d
NamTaeHee1 <skaxogml777@naver.com>
2021-06-10 22:00:33 +0900

    [ADD] TeamManager
    [ADD] TeamManager

---
+++ Assets/Scripts/Main/TeamManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] RedTeamList;
+    [SerializeField] GameObject[] BlueTeamList;
+}
